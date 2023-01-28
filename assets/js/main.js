4c6f60ced9853857e4b365b32504ce5f3fd16854
Graph1te <egor.anders23@gmail.com>
2023-01-28 17:33:15 +0500

    notifications

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   const awards = document.querySelectorAll('.table__awards');
 
-  awards.forEach(item => {
+  awards.forEach((item) => {
     const awardsList = item.querySelectorAll('.table__award');
     const length = awardsList.length;
 
@@ -11,3 +11,16 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 });
+
+const leftValue = document.querySelector('.slider__value--left');
+const rightValue = document.querySelector('.slider__value--right');
+for (let e of document.querySelectorAll('input[type="range"].slider-progress')) {
+  e.style.setProperty('--value', e.value);
+  e.style.setProperty('--min', e.min == '' ? '0' : e.min);
+  e.style.setProperty('--max', e.max == '' ? '100' : e.max);
+  e.addEventListener('input', () => {
+    e.style.setProperty('--value', e.value);
+    leftValue.textContent = e.value + '%';
+    rightValue.textContent = (100 - e.value).toFixed(1) + '%';
+  });
+}
